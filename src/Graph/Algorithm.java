4f185744ca0aecd1b6c3ede96cd2b21dc537ddff
wuxinran <wuxinran@mobike.com>
2018-05-15 21:18:46 +0800

    baseline

---
+++ src/Graph/Algorithm.java
@@ -0,0 +1,20 @@
+package Graph;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public interface Algorithm {
+	  /**
+	   * 执行算法
+	   */
+	  void perform(Graph g, String sourceVertex,String date,String time,String end_vertex);
+	  /**
+	   * 得到路径
+	   */
+	  Map<String, String> getPath();
+	}
+
