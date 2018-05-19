d778bff4dc20b70c26f00de9dd287d0af857b3bd
wuxinran <wuxinran@mobike.com>
2018-05-20 01:14:57 +0800

    add distance function

--- src/gov/bjjtw/lastTrain/CommonTools/CommonTools.java
+++ src/gov/bjjtw/lastTrain/CommonTools/CommonTools.java
@@ -1,11 +1,11 @@
 package gov.bjjtw.lastTrain.CommonTools;
 
 
-import java.io.IOException;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.ObjectInputStream;
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import static java.lang.Math.toRadians;
 
 public class CommonTools {
     public static int TransferTime(String time) {
@@ -28,7 +28,7 @@ public class CommonTools {
         return h+":"+m+":"+s;
     }
 
-    public static Object deepCopy(Object orig) {
+    public static Object DeepCopy(Object orig) {
         Object obj = null;
         try {
             // Write the object out to a byte array
@@ -48,6 +48,21 @@ public class CommonTools {
             cnfe.printStackTrace();
         }
         return obj;
+    }
+
+    public static double SimpleDist(double lat1, double lng1, double lat2, double lng2) {
+        // 经度差值
+        double dx = lng1 - lng2;
+        // 纬度差值
+        double dy = lat1 - lat2;
+        // 平均纬度
+        double b = (lat1 + lat2) / 2.0;
+        // 东西距离
+        double Lx = toRadians(dx) * 6367000.0* Math.cos(toRadians(b));
+        // 南北距离
+        double Ly = 6367000.0 * toRadians(dy);
+        // 用平面的矩形对角距离公式计算总距离
+        return Math.sqrt(Lx * Lx + Ly * Ly);
     }
 
 }
